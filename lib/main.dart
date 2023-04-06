1b0a58bdf2babc02c190690293e9b9d6a2da33b2
Aaldn <84779817+Aaldn@users.noreply.github.com>
2023-04-06 17:44:26 +0200

    Revert "Theme (#1)"
    This reverts commit 62e725c1622dceb287bd75dddd6ff08de01eb007.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,90 +10,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Made for FlexColorScheme version 7.0.0. Make sure you
-      // use same or higher package version, but still same major version.
-      // If you use a lower version, some properties may not be supported.
-      // In that case remove them after copying this theme to your app.
-      theme: FlexThemeData.light(
-        colors: const FlexSchemeColor(
-          primary: Color(0xffffa384),
-          primaryContainer: Color(0xffff7d51),
-          secondary: Color(0xff74bdcb),
-          secondaryContainer: Color(0xffe7f2f8),
-          tertiary: Color(0xffefe7bc),
-          tertiaryContainer: Color(0xffe5d893),
-          appBarColor: Color(0xffe7f2f8),
-          error: Color(0xffb00020),
-        ),
-        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-        blendLevel: 7,
-        appBarStyle: FlexAppBarStyle.primary,
-        subThemesData: const FlexSubThemesData(
-          useFlutterDefaults: true,
-          useTextTheme: true,
-          defaultRadius: 20.0,
-          textButtonRadius: 5.0,
-          filledButtonRadius: 5.0,
-          elevatedButtonRadius: 5.0,
-          outlinedButtonRadius: 5.0,
-          inputDecoratorSchemeColor: SchemeColor.secondary,
-          inputDecoratorUnfocusedBorderIsColored: false,
-        ),
-        keyColors: const FlexKeyColors(
-          useSecondary: true,
-          useTertiary: true,
-          keepPrimary: true,
-          keepSecondary: true,
-          keepTertiary: true,
-          keepPrimaryContainer: true,
-          keepSecondaryContainer: true,
-          keepTertiaryContainer: true,
-        ),
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-        // To use the playground font, add GoogleFonts package and uncomment
-        fontFamily: GoogleFonts.inter().fontFamily,
-      ),
-      darkTheme: FlexThemeData.dark(
-        colors: const FlexSchemeColor(
-          primary: Color(0xffffa384),
-          primaryContainer: Color(0xff000000),
-          secondary: Color(0xff74bdcb),
-          secondaryContainer: Color(0xff334b4f),
-          tertiary: Color(0xffefe7bc),
-          tertiaryContainer: Color(0xffe5d893),
-          appBarColor: Color(0xff334b4f),
-          error: Color(0xffcf6679),
-        ),
-        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-        blendLevel: 13,
-        subThemesData: const FlexSubThemesData(
-          useFlutterDefaults: true,
-          useTextTheme: true,
-          defaultRadius: 20.0,
-          textButtonRadius: 5.0,
-          filledButtonRadius: 5.0,
-          elevatedButtonRadius: 5.0,
-          outlinedButtonRadius: 5.0,
-          inputDecoratorSchemeColor: SchemeColor.secondary,
-          inputDecoratorUnfocusedBorderIsColored: false,
-        ),
-        keyColors: const FlexKeyColors(
-          useSecondary: true,
-          useTertiary: true,
-          keepPrimary: true,
-          keepSecondary: true,
-          keepTertiary: true,
-          keepPrimaryContainer: true,
-          keepSecondaryContainer: true,
-          keepTertiaryContainer: true,
-        ),
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-        // To use the Playground font, add GoogleFonts package and uncomment
-        fontFamily: GoogleFonts.inter().fontFamily,
-      ),
-      // If you do not have a themeMode switch, uncomment this line
-      // to let the device system mode control the theme mode:
-      themeMode: ThemeMode.light,
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Learn Flow Back-office"),
