049265f1fc2986ac0b1eafef4e61159437e985e1
Aaldn <aaldn.dev@gmail.com>
2023-05-03 19:39:23 +0200

    fix: elevated button onPressed()

--- lib/widgets/elevated_button.dart
+++ lib/widgets/elevated_button.dart
@@ -5,17 +5,17 @@ class MyElevatedButton extends ConsumerWidget {
   const MyElevatedButton({
     super.key,
     this.height = 50,
-    void Function()? this.onPressed,
+    this.onPressed,
     this.child,
   });
   final double height;
-  final Function? onPressed;
+  final VoidCallback? onPressed;
   final Widget? child;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: onPressed,
       style: Theme.of(context).elevatedButtonTheme.style!.copyWith(
             minimumSize: MaterialStatePropertyAll(
               Size(400, height),
