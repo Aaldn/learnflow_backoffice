4a209e7a0c761392ff89b0fb4c5d6aa4fa932eba
Aaldn <aaldn.dev@gmail.com>
2023-05-06 15:49:04 +0200

    refactor: rename model

--- lib/models/school_subject_taught.dart
+++ lib/models/school_subject_taught.dart
@@ -2,18 +2,18 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:learnflow_backoffice/models/school_subject.dart';
 import 'package:learnflow_backoffice/models/teacher.dart';
 
-part 'school_subject_teached.freezed.dart';
-part 'school_subject_teached.g.dart';
+part 'school_subject_taught.freezed.dart';
+part 'school_subject_taught.g.dart';
 
 @freezed
-class SchoolSubjectTeached with _$SchoolSubjectTeached {
-  factory SchoolSubjectTeached({
+class SchoolSubjectTaught with _$SchoolSubjectTaught {
+  factory SchoolSubjectTaught({
     required int? id,
     required int? yearsOfExperience,
     required Teacher? teacher,
     required SchoolSubject? schoolSubject,
-  }) = _SchoolSubjectTeached;
+  }) = _SchoolSubjectTaught;
 
-  factory SchoolSubjectTeached.fromJson(Map<String, dynamic> json) =>
-      _$SchoolSubjectTeachedFromJson(json);
+  factory SchoolSubjectTaught.fromJson(Map<String, dynamic> json) =>
+      _$SchoolSubjectTaughtFromJson(json);
 }
