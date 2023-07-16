6956899c25aeccaac1ee6d6f334456aad40d68d6
Aaldn <aaldn.dev@gmail.com>
2023-07-16 15:06:09 +0200

    refactor: update envied field

--- lib/services/env/env.dart
+++ lib/services/env/env.dart
@@ -5,6 +5,6 @@ part 'env.g.dart';
 @Envied(path: ".env.dev")
 abstract class Env {
   @Envied(obfuscate: true)
-  @EnviedField(varName: 'SERVER_URL')
-  static const serverUrl = _Env.serverUrl;
+  @EnviedField(varName: 'API_BASE_URL')
+  static const apiBaseUrl = _Env.apiBaseUrl;
 }
